258c6fc38e2ac0a4ee023f373f51742d8fce2d1e
ambaris <ambaris7@rediffmail.com>
2026-04-21 17:01:34 +0530

    fixed calc intf

--- src/main/java/com/maaza/myann/calc/Calculator.java
+++ src/main/java/com/maaza/myann/calc/Calculator.java
@@ -2,7 +2,7 @@ package com.maaza.myann.calc;
 
 public interface Calculator {
 
-    public float loss(final float[] arr1, final float[] arr2);
+    public float loss(float[] arr1, float[] arr2);
 
-    public void grad(final float[] arr1, final float[] arr2, final float[] arr3);
+    public void grad(float[] arr1, float[] arr2, float[] arr3);
 }
